b70d0ca537ebcf43922c13ec788e9214b9da4c53
그거 <dnrwjd0116@naver.com>
2024-02-21 13:00:27 +0000

    Comment: correct typos

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, "public")))
 //call api middleware
 app.use('/auth', authApi)
 
-//errorhandling middlewatre
+//errorhandling middleware
 app.use('/', errorHandler)
+
 module.exports = app
