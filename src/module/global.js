7da5ead0fbd604ef7e922e85291112511cc6df82
그거 <dnrwjd0116@naver.com>
2024-03-06 14:30:55 +0000

    Revise: revise imageUploader middleware, add multer error handling code

--- src/module/global.js
+++ src/module/global.js
@@ -8,5 +8,10 @@ module.exports = {
 	AWS_OPTION : {
 		REGION : 'ap-northeast-2',
 		ACL : 'public-read-write'
+	},
+	
+	MULTER_ERROR : {
+		UNEXPECTED_FILE : "LIMIT_UNEXPECTED_FILE",
+		FILE_SIZE : "LIMIT_FILE_SIZE"
 	}
 }
