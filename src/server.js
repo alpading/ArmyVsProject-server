e7de4ee6733a9887a6bb23cc343cb997e91d0926
그거 <dnrwjd0116@naver.com>
2024-03-10 13:58:57 +0000

    Feat: create temporary errorHandler

--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
+const cookieParser = require("cookie-parser")
 
 const authApi = require('./routes/auth')
+const elemApi = require('./routes/elem')
 
 const errorHandler = require('./middleware/errorHandler')
 
 //global middleware
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")))
+app.use(cookieParser())
 
 //call api middleware
 app.use('/auth', authApi)
+app.use('/elem', elemApi)
 
 //errorhandling middleware
 app.use('/', errorHandler)
