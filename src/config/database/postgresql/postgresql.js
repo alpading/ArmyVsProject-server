779eca99390be5d0259a8e1cdcc842842630dbd7
alpading <dnrwjd0116@naver.com>
2026-02-08 18:19:05 +0900

    Chore: add postgres service with docker-compose

--- src/config/database/postgresql/postgresql.js
+++ src/config/database/postgresql/postgresql.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const { Pool } = require("pg")
 
 const pool = new Pool({
-    user: process.env.POSTGRESQL_USER,
-    host: process.env.POSTGRESQL_HOST,
-    database: process.env.POSTGRESQL_DATABASE,
-    password: process.env.POSTGRESQL_PASSWORD,
-    port: process.env.POSTGRESQL_PORT,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
     max: 1000,
 	connectionTimeoutMillis: 3000
 })
