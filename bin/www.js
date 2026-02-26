a10282e727b000a9ace31af2335212ed4d92779e
alpading <dnrwjd0116@naver.com>
2026-02-26 20:26:08 +0900

    Style: clean up app.listen

--- bin/www.js
+++ bin/www.js
@@ -4,6 +4,6 @@ const env = require('../src/config/env')
 const app = require('../src/server')
 const port = env.PORT
 
-app.listen(port, (req, res) => {
-    console.log(port + '번 포트에서 서버 실행')
+app.listen(port, () => {
+    console.log(`${port}번 포트에서 서버 실행`)
 })
